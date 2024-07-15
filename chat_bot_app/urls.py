37d83351582268de7d531ee534b86b3ea9f8e933
rakesh kumar <rakesh@snakescript.com>
2024-07-15 13:14:25 +0530

    speech recogision completed

--- chat_bot_app/urls.py
+++ chat_bot_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import chat_view
+from .views import chat_view, chatbot_ajax
 
 urlpatterns = [
     path('', chat_view, name='chat_view'),
+    path('chatbot_ajax/', chatbot_ajax, name='chatbot_ajax'),
 ]
+
--- chat_bot_app/views.py
+++ chat_bot_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .forms import ChatForm
+from django.http import JsonResponse
+
 from .models import Message
 from .utils import get_response_from_model
 
@@ -7,22 +8,35 @@ from .utils import get_response_from_model
 
 def chat_view(request):
     if request.method == 'POST':
-        form = ChatForm(request.POST)
-        if form.is_valid():
-            user_message = form.cleaned_data['message']
-            
+        user_message = request.POST.get('query')
+        if user_message is not None:
+        
             bot_response = get_response_from_model(user_message)
 
             Message.objects.create(user=request.user, message=user_message, response=bot_response)
             return redirect('chat_view')
-    else:
-        form = ChatForm()
-
+    
     messages = Message.objects.filter(user=request.user).order_by('-timestamp')
     
     context = {
-            'form': form, 
             'messages': messages
         }
     
     return render(request, 'chat_bot_app/chat_bot_1.html', context)
+
+
+
+def chatbot_ajax(request):
+    if request.method == 'POST':
+        user_message = request.POST.get('query')
+
+        if user_message is not None:
+            bot_response = get_response_from_model(user_message)
+
+            Message.objects.create(user=request.user, message=user_message, response=bot_response)
+            
+            data = {
+                'response': bot_response,
+            }
+            
+            return JsonResponse(data)
